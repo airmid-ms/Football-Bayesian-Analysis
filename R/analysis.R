e32a9d8d02fb31d871585f7b32b848813a6b50a7
airmid-ms <a.murphyscott@gmail.com>
2026-06-13 22:49:38 +0100

    bayesian model and ppc checks, attempting side by side ppc plots

--- R/analysis.R
+++ R/analysis.R
@@ -1,12 +1,14 @@
 # libraries
 library(tidyverse)
 library(tidybayes)
+library(dplyr)
 library(readr)
 library(ggplot2)
 library(rjags)
 library(R2jags)
 library(tidybayes)
 library(bayesplot) 
+library(patchwork)
 
 
 # load data
@@ -18,3 +20,120 @@ spread_vs_outcome <- ggplot(football_dat, aes(x=spread, y=outcome)) +
   geom_point(position = "jitter")
 spread_vs_outcome
 ggsave("output/plots/spread_vs_outcome.png", plot = spread_vs_outcome, width = 8, height = 6)
+
+# model
+
+football_model <- "
+ model {
+  
+    # Likelihood
+    for (i in 1:n_game) {
+    y.i[i] ~ dnorm(mu.i[i], sigma[i]^-2) 
+    mu.i[i] <- alpha + beta*(x.i[i])
+    sigma[i] <- exp(alpha_sigma + beta_sigma*x.i[i])
+    }
+    
+    # Prior
+    alpha ~ dnorm(0, 10^-2) 
+    beta ~ dnorm(0, 10^-2) 
+    alpha_sigma ~ dnorm(0, 10^-2) 
+    beta_sigma ~ dnorm(0, 10^-2) 
+  
+ }
+" 
+
+## A standard linear regression would not be able to tell whether a larger spread makes the outcome more predictable. 
+## So a heteroscedastic regression was used instead so that both the mean and variance of the outcome are modeled as opposed to just the mean.
+
+football_data <- list(
+  y.i = football_dat$outcome,
+  x.i = football_dat$spread,
+  n_game = nrow(football_dat))
+
+
+params <- c("alpha", "beta", "alpha_sigma", "beta_sigma") 
+
+mod <- jags(data = football_data,
+            parameters.to.save = params,
+            n.iter = 4000,
+            n.burnin = 2000,
+            model.file = textConnection(football_model))
+plot(mod)
+
+mod$BUGSoutput$summary[c("alpha", "beta", "alpha_sigma", "beta_sigma"), ]
+
+
+## model summary
+## alpha: Median = 0.22, Cred Int = (-0.77, 1.19) beta: Median = 1, Cred Int = (0.84, 1.17) alpha_sigma: Median = 2.66, Cred Int = (2.61, 2.71) beta_sigma: Median = -0.0076, Cred Int = (-0.016, 0.0007)
+
+## There does appear to be a relationship between the outcome and spread. The credible interval does not include 0 and so there appears to be a significant relationship between outcome and point spread. The median is positive, indicating that when point spread increases so does the outcome.
+
+## There does not appear to be a relationship between the residual variation and spread. The credible interval for beta sigma includes 0 and the median is very close to 0, there does not appear to be a relationship between residual variation and point spread.
+
+
+
+# Posterior Predictive Checking
+football_model <- "
+ model {
+  
+    # Likelihood
+    for (i in 1:n_game) {
+    y.i[i] ~ dnorm(mu.i[i], sigma[i]^-2) 
+    mu.i[i] <- alpha + beta*(x.i[i])
+    sigma[i] <- exp(alpha_sigma + beta_sigma*x.i[i])
+    }
+    
+    # Prior
+    alpha ~ dnorm(0, 10^-2) 
+    beta ~ dnorm(0, 10^-2) 
+    alpha_sigma ~ dnorm(0, 10^-2) 
+    beta_sigma ~ dnorm(0, 10^-2) 
+    
+    # Posterior prediction
+    for(i in 1:n_game) {
+    yrep[i] ~ dnorm(mu.i[i], sigma[i]^-2)
+    }
+  
+}
+"  
+football_data <- list(
+  y.i = football_dat$outcome, 
+  x.i = football_dat$spread,
+  n_game = nrow(football_dat))
+
+
+params <- c("alpha", "beta", "alpha_sigma", "beta_sigma", "yrep") 
+
+mod <- jags(data = football_data,
+            parameters.to.save = params,
+            n.iter = 4000,
+            n.burnin = 2000,
+            model.file = textConnection(football_model))
+
+plot(mod)
+
+mod$BUGSoutput$summary[c("alpha", "beta", "alpha_sigma", "beta_sigma"), ]
+
+## extracting observed outcomes, simulated replicates and predictor for PPC
+y <- football_dat$outcome
+yrep <- mod$BUGSoutput$sims.list$yrep
+x <- football_dat$spread
+
+## minimum test stat
+d <- function(x) min(x)
+
+## comparing distribution of simulated min to observed min
+ppc1 <- ppc_stat(y, yrep, stat = "d")
+ggsave("output/plots/ppc_minimum.png", plot = ppc1, width = 8, height = 6)
+
+## The PPC stat plot shows that the observed minimum is within the simulated minimum distribution. This shows the model is generating simulated data similar to the observed data.
+
+
+## comparing simulated median/intervals against the spread
+ppc2 <- ppc_intervals(y, yrep, x)
+ggsave("output/plots/ppc_intervals.png", plot = ppc2, width = 8, height = 6)
+
+## The PPC intervals plot shows that the model manages to capture a decent range of the observed values variability. The models uncertainty does change in relation to the spread units.
+
+combined <- ppc1 + ppc2
+ggsave("output/plots/ppc_combined.png", plot = combined, width = 12, height = 5)
--- README.md
+++ README.md
@@ -6,10 +6,30 @@ Before a game, experts predict how many points the "better team" should win by. 
 > 1) Does the spread predict the average outcome?
 > 2) Does the spread predict how variable the outcome is?
 
+---
+
 ## Initial Exploration
 ![Spread vs Outcome](output/plots/spread_vs_outcome.png)
 
 There appears to be a **positive** relationship between spread (predictor) and outcome (response) which suggests the experts involved are good at predicting results. However, there are outcomes with a negative value indicating that the perceived "worse team" do win despite what the experts think.
 
 The variability in outcomes looks consistent across different spread values.
+
+--
+
+## Bayesian Regression Model & Inference
+A heteroscedastic model was used instead of a standard linear regression model to explore whether the variability of outcomes changes with the spread, rather than assuming it stays the same.
+
+Using this model it is found that:
+1. The spread strongly predicts the outcome, for every point of spread the expected outcome increases by ~ 1 point.
+2. There is little to no evidence that the variability of the outcome changes with the spread. High spread games (large difference in strength between teams) do not appear to be more predictable than low spread games.
+
+## Model Validation
+For Bayesian analysis, posterior predictive checks (PPCs) confirm that the model is capable of producing simulated data based on the observed data and thus is valid in its prediction.
+
+![PPC Minimum Check & Interbals](output/plots/ppc_combined.png)
+
+Posterior predictive checks confirm:
+1. The model generates simulated data consistent with the observed data.
+2. The models uncertainty appropriately accounts for different spread values.
 
